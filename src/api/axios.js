9e9db7a7dd0f33223b19f87c8dfb1270a793a755
Priya-06-12 <priyaangel61003@gmail.com>
2025-08-28 14:11:00 +0530

    added

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,21 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   // baseURL: "/api/v1", 
+//   baseURL: "https://e-commerce-backend2-1.onrender.com", 
+//   withCredentials: true, 
+// });
+
+// export default api;
+
+
+
+
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "/api/v1", 
-  baseURL: "https://e-commerce-backend2-1.onrender.com", 
-  withCredentials: true, 
+  baseURL: process.env.REACT_APP_BACKEND_URL + "/api/v1",
+  withCredentials: true,
 });
 
 export default api;
